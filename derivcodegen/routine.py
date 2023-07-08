4d3976dc28bafb18b39c5617755f03faf1c98dc4
Mark Dewing <markdewing@gmail.com>
2023-07-07 20:58:55 -0500

    Add variable name to debug output

--- derivcodegen/routine.py
+++ derivcodegen/routine.py
@@ -71,6 +71,8 @@ class Routine:
                         str(s.lhs),
                         "=",
                         str(s.rhs),
+                        " wrt ",
+                        var,
                         " free symbols: ",
                         s.rhs.free_symbols,
                     )
